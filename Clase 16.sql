a5cac4ad451eee30d0404441b0066c1c18ee5e48
Francisco-Acuna <140303345+Francisco-Acuna@users.noreply.github.com>
2023-11-30 01:59:46 -0300

    Add files via upload

---
+++ Clase 16.sql
@@ -0,0 +1,7 @@
+-- Clase 16
+
+-- 2023-11-30 En la clase de hoy se hicieron las evaluaciones de los TPs finales entregados
+-- Acabo de subir al Git-Hub los 3 archivos que componen la totalidad del ejercicio:
+-- script para crear la base, script para insertar los resgitros y script para resolver los ejercicios.
+-- Por favor, revisen con lo presentado y me consultan las dudas que tengan.
+-- Aquellos que tengan que volver a presentar sus TPs miren bien lo subido y corrijan en donde haga falta.
